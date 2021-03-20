5f2166ffee227bf2cbedc61ab3f4a103c855d99e
peiceofnerd <dari.dorenska@gmail.com>
2021-03-20 08:12:14 +0200

    pratice with git and interfaces

---
+++ unit_3_oop/application/src/main/java/ua/com/alevel/Application.java
@@ -0,0 +1,10 @@
+package ua.com.alevel;
+
+import ua.com.alevel.view.ApplicationView;
+
+public class Application {
+    public static void main(String[] args) {
+
+        ApplicationView.viewApplication();
+    }
+}
